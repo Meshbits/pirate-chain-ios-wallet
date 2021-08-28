b035c2d617e2ff4a2ccb0c7cf22b3b0f0a9aa475
Lokesh Sehgal <lokeshsehgal@Lokeshs-MacBook-Pro.local>
2021-08-29 00:54:48 +0530

    Layout adjustments on WordsVerificationScreen

--- wallet/wallet/Views/GenerateKeyPhrase/WordsVerificationScreen.swift
+++ wallet/wallet/Views/GenerateKeyPhrase/WordsVerificationScreen.swift
@@ -13,11 +13,12 @@ struct WordsVerificationScreen: View {
     var body: some View {
         ZStack{
             ARRRBackground().edgesIgnoringSafeArea(.all)
-            Text("Confirm Recovery Phrase").padding(.trailing,80).padding(.leading,80).foregroundColor(.white).multilineTextAlignment(.center).lineLimit(nil).font(.barlowRegular(size: Device.isLarge ? 36 : 28)).padding(.top,80)
-            
-            Text("Almost done! Enter the following words from your recovery phrase").padding(.trailing,60).padding(.leading,60).foregroundColor(.gray).multilineTextAlignment(.center).foregroundColor(.gray).padding(.top,10).font(.barlowRegular(size: Device.isLarge ? 20 : 14))
-            Spacer()
-            Spacer()
+            VStack{
+                Text("Confirm Recovery Phrase").padding(.trailing,80).padding(.leading,80).foregroundColor(.white).multilineTextAlignment(.center).lineLimit(nil).font(.barlowRegular(size: Device.isLarge ? 36 : 28)).padding(.top,80)
+                Text("Almost done! Enter the following words from your recovery phrase").padding(.trailing,60).padding(.leading,60).foregroundColor(.gray).multilineTextAlignment(.center).foregroundColor(.gray).padding(.top,10).font(.barlowRegular(size: Device.isLarge ? 20 : 14))
+                Spacer()
+                Spacer()
+            }
         }
         
     }
