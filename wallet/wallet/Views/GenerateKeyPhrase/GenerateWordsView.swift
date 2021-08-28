bd46559e54d4e4148bd5e32d7c405d35fd1dcb17
Lokesh Sehgal <lokeshsehgal@Lokeshs-MacBook-Pro.local>
2021-08-28 16:00:28 +0530

    Changes in the logics for generation of words

--- wallet/wallet/Views/GenerateKeyPhrase/GenerateWordsView.swift
+++ wallet/wallet/Views/GenerateKeyPhrase/GenerateWordsView.swift
@@ -41,55 +41,55 @@ enum Words: Int {
     var id: Int {
         switch self {
         case .word_one:
-            return 0
-        case .word_two:
             return 1
-        case .word_three:
+        case .word_two:
             return 2
-        case .word_four:
+        case .word_three:
             return 3
-        case .word_five:
+        case .word_four:
             return 4
-        case .word_six:
+        case .word_five:
             return 5
-        case .word_seven:
+        case .word_six:
             return 6
-        case .word_eight:
+        case .word_seven:
             return 7
-        case .word_nine:
+        case .word_eight:
             return 8
-        case .word_ten:
+        case .word_nine:
             return 9
-        case .word_eleven:
+        case .word_ten:
             return 10
-        case .word_twelve:
+        case .word_eleven:
             return 11
-        case .word_thirteen:
+        case .word_twelve:
             return 12
-        case .word_fourteen:
+        case .word_thirteen:
             return 13
-        case .word_fifteen:
+        case .word_fourteen:
             return 14
-        case .word_sixteen:
+        case .word_fifteen:
             return 15
-        case .word_seventeen:
+        case .word_sixteen:
             return 16
-        case .word_eighteen:
+        case .word_seventeen:
             return 17
-        case .word_nineteen:
+        case .word_eighteen:
             return 18
-        case .word_twenty:
+        case .word_nineteen:
             return 19
-        case .word_twenty_one:
+        case .word_twenty:
             return 20
-        case .word_twenty_two:
+        case .word_twenty_one:
             return 21
-        case .word_twenty_three:
+        case .word_twenty_two:
             return 22
-        case .word_twenty_four:
+        case .word_twenty_three:
             return 23
-        case .word_next:
+        case .word_twenty_four:
             return 24
+        case .word_next:
+            return 25
         }
     }
     
@@ -119,7 +119,6 @@ final class GenerateWordsViewModel: ObservableObject {
             
             mWordTitle = randomKeyPhrase![0]
             
-            print("randomKeyPhrase")
             print(randomKeyPhrase)
         } catch {
             // Handle error in here
@@ -132,7 +131,7 @@ final class GenerateWordsViewModel: ObservableObject {
         
         mVisibleWord.previous()
         
-        mWordIndex = mVisibleWord.rawValue - 1
+        mWordIndex = mVisibleWord.rawValue + 1
         
         mWordTitle = randomKeyPhrase![mVisibleWord.rawValue]
     }
@@ -182,7 +181,7 @@ struct GenerateWordsView: View {
             
             
             ARRRBackButton(action: {
-                if self.viewModel.mWordIndex == 0 {
+                if self.viewModel.mWordIndex == 1 {
                     presentationMode.wrappedValue.dismiss()
                 }else{
                     self.viewModel.backPressedToPopBack()
