e490abf216113976ca5587d1d15a1eb2a0e0ab79
Lokesh Sehgal <lokeshsehgal@Lokeshs-MacBook-Pro.local>
2021-11-12 15:32:36 +0530

    Fixed - Recovery Screen Popup, whenever resume an APP #41
    https://github.com/Meshbits/pirate-chain-ios-wallet/issues/41

--- wallet/wallet/Views/PasscodeValidationScreen.swift
+++ wallet/wallet/Views/PasscodeValidationScreen.swift
@@ -184,7 +184,9 @@ struct PasscodeValidationScreen: View {
                                 guard isDismiss else { return }
                                 if (isDismiss){
                                     self.presentationMode.wrappedValue.dismiss()
-                                    NotificationCenter.default.post(name: NSNotification.Name("PasscodeValidationSuccessful"), object: nil)
+                                    if !isAuthenticationEnabled {
+                                        NotificationCenter.default.post(name: NSNotification.Name("PasscodeValidationSuccessful"), object: nil)
+                                    }
                                 }
                             }
             )
