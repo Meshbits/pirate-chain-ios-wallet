1a04cb50c82712dc98a482888cdfdec0cbf0d69d
Lokesh Sehgal <lokeshsehgal@Lokeshs-MacBook-Pro.local>
2021-11-16 16:09:54 +0530

    Minor updates in swipe layout of recovery phrase

--- wallet/wallet/Views/GenerateKeyPhrase/GenerateWordsView.swift
+++ wallet/wallet/Views/GenerateKeyPhrase/GenerateWordsView.swift
@@ -171,14 +171,29 @@ struct GenerateWordsView: View {
                     .padding(.top,20)
                 Text("Write down the following words in order".localized()).padding(.trailing,60).padding(.leading,60).foregroundColor(.gray).multilineTextAlignment(.center).foregroundColor(.gray).padding(.top,10).scaledFont(size: 15)
                 Spacer()
-                Text(self.viewModel.mWordTitle).transition(.move(edge: isForward ? .trailing : .leading)).id("titleComponentID" + self.viewModel.mWordTitle).padding(.trailing,40).padding(.leading,40).foregroundColor(.white).multilineTextAlignment(.center).lineLimit(nil)
-                    .scaledFont(size: 35)
+                Text(self.viewModel.mWordTitle)/*.transition(.move(edge: isForward ? .trailing : .leading))*/.id("titleComponentID" + self.viewModel.mWordTitle).padding(.trailing,40).padding(.leading,40).foregroundColor(.white).multilineTextAlignment(.center).lineLimit(nil)
+//                    .transition(
+//                        .asymmetric(
+//                            insertion: .move(edge: isForward ? .trailing : .leading),
+//                            removal: .move(edge: isForward ? .leading : .trailing)
+//                        )
+//                    )
+//                    .animation(.default)
+//                    .id(UUID())
                     .padding(.top,80)
                 Text("\(self.viewModel.mWordIndex) of 24").padding(.trailing,60).padding(.leading,60).foregroundColor(.gray).multilineTextAlignment(.center).foregroundColor(.gray).padding(.top,10)
                     .scaledFont(size: 17)
                 Spacer()
                 
-                Spacer()
+//                Image(self.viewModel.mWordIndex%2==0 ? "leftbgwords" : "rightbgwords")
+//                    .transition(
+//                    .asymmetric(
+//                        insertion: .move(edge: isForward ? .trailing : .leading),
+//                        removal: .move(edge: isForward ? .leading : .trailing)
+//                    )
+//                )
+//                .animation(.default)
+//                .id(UUID())
                 
                 Text("For security purposes, do not screeshot or email these words.".localized()).padding(.trailing,40).padding(.leading,40).foregroundColor(.gray).multilineTextAlignment(.leading).foregroundColor(.gray).padding(.top,10)
                     .scaledFont(size: 12)
@@ -223,15 +238,16 @@ struct GenerateWordsView: View {
             
         }
         .highPriorityGesture(DragGesture(minimumDistance: 3.0, coordinateSpace: .local).onEnded { value in
-            print(value.translation)
-           
-            if value.translation.width < 0 && value.translation.height > -30 && value.translation.height < 30 {
+            
+            if value.translation.width < 0 && value.translation.height > -80 && value.translation.height < 80 {
 //                withAnimation(.easeInOut(duration: 0.2), {
+                    self.isForward = true
                     self.viewModel.updateLayoutTextOrMoveToNextScreen()
 //               })
             }
-            else if value.translation.width > 0 && value.translation.height > -30 && value.translation.height < 30 {
+            else if value.translation.width > 0 && value.translation.height > -80 && value.translation.height < 80 {
 //                withAnimation(.easeIn(duration: 0.2), {
+                    self.isForward = false
                     self.viewModel.backPressedToPopBack()
 //               })
             }
