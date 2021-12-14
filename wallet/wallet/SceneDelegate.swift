bfabeb2a8c76f5f844f28230691106706623e0b7
Lokesh Sehgal <lokeshsehgal@Lokeshs-MacBook-Pro.local>
2021-12-14 10:22:51 +0530

    Blur effect style changes.

--- wallet/wallet/SceneDelegate.swift
+++ wallet/wallet/SceneDelegate.swift
@@ -126,7 +126,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
         
-        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.regular)
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = window!.frame
         blurEffectView.tag = mBlurrViewTag
