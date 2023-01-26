8cada36e0020625fc5b3d797b392987e9ee3f605
Lokesh Sehgal <lastw0nd3r@gmail.com>
2023-01-26 16:45:50 +0530

    Changed config for ARRR wallet endpoints and minor fix in async flow for presend

--- wallet/wallet/Environment/ZECCWalletEnvironment.swift
+++ wallet/wallet/Environment/ZECCWalletEnvironment.swift
@@ -29,7 +29,7 @@ final class ZECCWalletEnvironment: ObservableObject {
 
     static var shared: ZECCWalletEnvironment = try! ZECCWalletEnvironment() // app can't live without this existing.
     static let memoLengthLimit: Int = 512
-    static let defaultLightWalletEndpoint = "lightd.pirate.black"
+    static let defaultLightWalletEndpoint = ZcashSDK.isMainnet ? "lightd1.pirate.black" : "testlightd.pirate.black" //"lightd.pirate.black"
     static let defaultLightWalletPort: Int = 443
 //    static let defaultFee = 10_000 // Earlier we have used ZcashSDK.defaultFee()
     @Published var state: WalletState
